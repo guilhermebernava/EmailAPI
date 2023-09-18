0e5cb9e866723b54bef123adf391d094dea3c443
guilherme bernava <guilhermebernava00@gmail.com>
2023-09-18 13:05:09 -0300

    add some changes

--- API/Queries/Query.cs
+++ API/Queries/Query.cs
@@ -1,5 +1,12 @@
-﻿namespace API.Queries;
+﻿using HotChocolate.Authorization;
+
+namespace API.Queries;
 
 public class Query
 {
+    [Authorize]
+    public async Task<bool> Test()
+    {
+        return true;
+    }
 }
